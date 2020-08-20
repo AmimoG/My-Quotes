0a32016ad84629b8c859abdad8239d2bd54a438b
AmimoG <gilbertmatete6@gmail.com>
2020-08-20 11:19:31 +0300

    Add Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyQuotesComponent } from './my-quotes/my-quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyQuotesComponent
+    MyQuotesComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
